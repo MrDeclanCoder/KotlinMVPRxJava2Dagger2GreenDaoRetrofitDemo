1634a7e3af322d852f488af2f2617d5fdba17594
MrDeclanCoder <dch123654>
2017-04-18 23:50:28 +0800

    RxJava2 splash daojishi

--- app/src/main/java/com/dch/test/ui/SplashActivity.java
+++ app/src/main/java/com/dch/test/ui/SplashActivity.java
@@ -7,12 +7,22 @@ import android.content.Intent;
 import android.os.Handler;
 import android.transition.Explode;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dch.test.R;
 import com.dch.test.base.BaseActivity;
 import com.dch.test.util.StatusBarUtils;
 
+import java.util.concurrent.TimeUnit;
+
 import butterknife.BindView;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 /**
  * 作者：Dch on 2017/4/10 18:37
@@ -21,16 +31,19 @@ import butterknife.BindView;
  */
 public class SplashActivity extends BaseActivity {
 
-    @BindView(R.id.imageView_splash)
+    @BindView(R.id.imageview_splash)
     ImageView imageView_splash;
+
+    @BindView(R.id.textView2)
+    TextView textView2;
 
     private Handler handler;
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
             setupWindowAnimations();
-            startActivity(new Intent(SplashActivity.this, GuideActivity.class));
-            finish();
+//            startActivity(new Intent(SplashActivity.this, GuideActivity.class));
+//            finish();
         }
     };
 
@@ -57,6 +70,44 @@ public class SplashActivity extends BaseActivity {
         animatorSet.setDuration(4000);
         animatorSet.play(alpha).with(scaleX).with(scaleY);
         animatorSet.start();
+
+        long count = 3;
+        Observable.interval(1, 1, TimeUnit.SECONDS)
+                .take(count + 1)
+                .map(new Function<Long, Long>() {
+                    @Override
+                    public Long apply(@NonNull Long i) throws Exception {
+                        return count - i;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(@NonNull Disposable disposable) throws Exception {
+
+                    }
+                })
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {}
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        textView2.setText(String.valueOf(aLong));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        setupWindowAnimations();
+                        startActivity(new Intent(SplashActivity.this, GuideActivity.class));
+                        finish();
+                    }
+                });
     }
 
     @Override
