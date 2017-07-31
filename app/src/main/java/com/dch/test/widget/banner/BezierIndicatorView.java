99f68c0750d989e04a7070321debf8917cb53012
MrDeclanCoder <dch123654>
2017-07-31 23:26:40 +0800

    自定义贝塞尔效果指示器indicator

--- app/src/main/java/com/dch/test/widget/banner/BezierIndicatorView.java
+++ app/src/main/java/com/dch/test/widget/banner/BezierIndicatorView.java
@@ -21,8 +21,8 @@ import java.util.List;
  */
 public class BezierIndicatorView extends View implements ViewPager.OnPageChangeListener {
     private final String TAG = "BezierIndicatorView";
-
     private final float RATIO = 0.551915024494f;
+    private boolean mUnderHalfDrawing = true;
     private int mDefaultIndicatorColor = Color.BLUE;
     private int mMoveIndicatorColor = Color.RED;
     private int mIndicatorCount = 5;
@@ -30,7 +30,6 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
     private float mIndicatorRadius = 15;
     float deltLittle = mIndicatorRadius * RATIO;
     float delt = deltLittle;
-    float deltBigger = (mIndicatorRadius + 10) * RATIO;
     private float mIndicatorInterval = 10;
     private int mWidth;
     private int mHeight;
@@ -73,7 +72,6 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
 
     public BezierIndicatorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
         init(context, attrs);
     }
 
@@ -93,63 +91,6 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
         mCircleLinePaint.setColor(mDefaultIndicatorColor);
         mCircleLinePaint.setStyle(Paint.Style.STROKE);
         mCircleLinePaint.setStrokeWidth(mIndicatorRadius * 2);
-
-
-    }
-
-    public void setIndicatorList(int count) {
-        if (mDefaultIndicatorPaths.size() > 0) mDefaultIndicatorPaths.clear();
-
-        this.mIndicatorCount = count;
-        for (int i = 0; i < mIndicatorCount; i++) {
-
-            Indicator indicator = new Indicator();
-            indicator.point0.x = mIndicatorRadius * (i * 2 + 1) + (i + 1) * mIndicatorInterval;
-            indicator.point0.y = mHeight / 2 + mIndicatorRadius;
-
-            indicator.point1.x = mIndicatorRadius * (i * 2 + 1) + (i + 1) * mIndicatorInterval + delt;
-            indicator.point1.y = mHeight / 2 + mIndicatorRadius;
-
-            indicator.point2.x = mIndicatorRadius * (i * 2 + 2) + (i + 1) * mIndicatorInterval;
-            indicator.point2.y = mHeight / 2 + delt;
-
-            indicator.point3.x = mIndicatorRadius * (i * 2 + 2) + (i + 1) * mIndicatorInterval;
-            indicator.point3.y = mHeight / 2;
-
-            indicator.point4.x = mIndicatorRadius * (i * 2 + 2) + (i + 1) * mIndicatorInterval;
-            indicator.point4.y = mHeight / 2 - delt;
-
-
-            indicator.point5.x = mIndicatorRadius * (i * 2 + 1) + (i + 1) * mIndicatorInterval + delt;
-            indicator.point5.y = mHeight / 2 - mIndicatorRadius;
-
-            indicator.point6.x = mIndicatorRadius * (i * 2 + 1) + (i + 1) * mIndicatorInterval;
-            indicator.point6.y = mHeight / 2 - mIndicatorRadius;
-
-            indicator.point7.x = mIndicatorRadius * (i * 2 + 1) + (i + 1) * mIndicatorInterval - delt;
-            indicator.point7.y = mHeight / 2 - mIndicatorRadius;
-
-            indicator.point8.x = mIndicatorRadius * (i * 2) + (i + 1) * mIndicatorInterval;
-            indicator.point8.y = mHeight / 2 - delt;
-
-            indicator.point9.x = mIndicatorRadius * (i * 2) + (i + 1) * mIndicatorInterval;
-            indicator.point9.y = mHeight / 2;
-
-            indicator.point10.x = mIndicatorRadius * (i * 2) + (i + 1) * mIndicatorInterval;
-            indicator.point10.y = mHeight / 2 + delt;
-
-            indicator.point11.x = mIndicatorRadius * (i * 2 + 1) + (i + 1) * mIndicatorInterval - delt;
-            indicator.point11.y = mHeight / 2 + mIndicatorRadius;
-
-            mIndicatorList.add(indicator);
-
-            mDefaultIndicatorPaths.add(calculatePath(indicator));
-            if (i == mCurrentIndicatorPosition) {
-                mCurrentPath = calculatePath(indicator);
-            }
-        }
-        mCirclePaths = mDefaultIndicatorPaths;
-        invalidate();
     }
 
     public void setIndicatorList2(int count) {
@@ -166,11 +107,11 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
         indicator5 = generateIndicator(4);
         indicator55 = generateIndicator(4);
 
-        path1 = calculatePath(indicator1);
-        path2 = calculatePath(indicator2);
-        path3 = calculatePath(indicator3);
-        path4 = calculatePath(indicator4);
-        path5 = calculatePath(indicator5);
+        path1 = calculateUnderHalfPath(indicator1);
+        path2 = calculateUnderHalfPath(indicator2);
+        path3 = calculateUnderHalfPath(indicator3);
+        path4 = calculateUnderHalfPath(indicator4);
+        path5 = calculateUnderHalfPath(indicator5);
 
         mDefaultIndicatorPaths.add(path1);
         mDefaultIndicatorPaths.add(path2);
@@ -178,7 +119,7 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
         mDefaultIndicatorPaths.add(path4);
         mDefaultIndicatorPaths.add(path5);
 
-        mCurrentPath = calculatePath(indicator1);
+        mCurrentPath = calculateUnderHalfPath(indicator1);
         mCirclePaths = mDefaultIndicatorPaths;
         invalidate();
     }
@@ -225,14 +166,29 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
         return indicator;
     }
 
-    private Path calculatePath(Indicator indicator) {
+    private Path calculateUnderHalfPath(Indicator indicator) {
         Path mBezierPath = new Path();
-        mBezierPath.reset();
         mBezierPath.moveTo(indicator.point0.x, indicator.point0.y);
         mBezierPath.cubicTo(indicator.point1.x, indicator.point1.y, indicator.point2.x, indicator.point2.y, indicator.point3.x, indicator.point3.y);
         mBezierPath.cubicTo(indicator.point4.x, indicator.point4.y, indicator.point5.x, indicator.point5.y, indicator.point6.x, indicator.point6.y);
         mBezierPath.cubicTo(indicator.point7.x, indicator.point7.y, indicator.point8.x, indicator.point8.y, indicator.point9.x, indicator.point9.y);
         mBezierPath.cubicTo(indicator.point10.x, indicator.point10.y, indicator.point11.x, indicator.point11.y, indicator.point0.x, indicator.point0.y);
+        mBezierPath.close();
+        return mBezierPath;
+    }
+
+    private Path calculateAboveHalfPath(Indicator preIndicator, Indicator nextIndicator) {
+        float middleX = (preIndicator.point2.x + mIndicatorInterval/2);
+        float topY = mHeight/2 -  mOffSet * mIndicatorRadius;
+        float bottomY = mHeight/2 + mOffSet* mIndicatorRadius;
+        Path mBezierPath = new Path();
+        mBezierPath.moveTo(preIndicator.point0.x, preIndicator.point0.y);
+        mBezierPath.quadTo(middleX,bottomY,nextIndicator.point0.x,nextIndicator.point0.y);
+        mBezierPath.cubicTo(nextIndicator.point1.x,nextIndicator.point1.y,nextIndicator.point2.x,nextIndicator.point2.y,nextIndicator.point3.x,nextIndicator.point3.y);
+        mBezierPath.cubicTo(nextIndicator.point4.x,nextIndicator.point4.y,nextIndicator.point5.x,nextIndicator.point5.y,nextIndicator.point6.x,nextIndicator.point6.y);
+        mBezierPath.quadTo(middleX,topY,preIndicator.point6.x,preIndicator.point6.y);
+        mBezierPath.cubicTo(preIndicator.point7.x, preIndicator.point7.y, preIndicator.point8.x, preIndicator.point8.y, preIndicator.point9.x, preIndicator.point9.y);
+        mBezierPath.cubicTo(preIndicator.point10.x, preIndicator.point10.y, preIndicator.point11.x, preIndicator.point11.y, preIndicator.point0.x, preIndicator.point0.y);
         mBezierPath.close();
         return mBezierPath;
     }
@@ -274,21 +230,31 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
 //            canvas.drawPath(mDefaultIndicatorPaths.get(i), mDefaultIndicatorPaint);
 //        }
 
-        canvas.drawPath(path1, mDefaultIndicatorPaint);
-        canvas.drawPath(path2, mDefaultIndicatorPaint);
-        canvas.drawPath(path3, mDefaultIndicatorPaint);
-        canvas.drawPath(path4, mDefaultIndicatorPaint);
-        canvas.drawPath(path5, mDefaultIndicatorPaint);
-
-
+        if (mUnderHalfDrawing){
+            canvas.drawPath(path1, mDefaultIndicatorPaint);
+            canvas.drawPath(path2, mDefaultIndicatorPaint);
+            canvas.drawPath(path3, mDefaultIndicatorPaint);
+            canvas.drawPath(path4, mDefaultIndicatorPaint);
+            canvas.drawPath(path5, mDefaultIndicatorPaint);
+        }else {
+            canvas.drawPath(path1, mDefaultIndicatorPaint);
+            canvas.drawPath(path2, mDefaultIndicatorPaint);
+            canvas.drawPath(path3, mDefaultIndicatorPaint);
+            canvas.drawPath(path4, mDefaultIndicatorPaint);
+            canvas.drawPath(path5, mDefaultIndicatorPaint);
+        }
         canvas.drawPath(mCurrentPath, mMoveIndicatorPaint);
-
     }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
         mCurrentIndicatorPosition = position;
         mOffSet = positionOffset;
+        if (mOffSet < 0.5) {
+            mUnderHalfDrawing = true;
+        } else {
+            mUnderHalfDrawing = false;
+        }
         invalidatePath();
     }
 
@@ -309,76 +275,83 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
         if (indicator1 == null) {
             return;
         }
+        if (mUnderHalfDrawing){
+            if (mCurrentIndicatorPosition == 0) {
 
-        if (mOffSet<0.5){
-            delt = deltLittle;
+                indicator1.point2.x = indicator11.point2.x + mIndicatorInterval * mOffSet;
+                indicator1.point3.x = indicator11.point2.x + mIndicatorInterval * mOffSet;
+                indicator1.point4.x = indicator11.point2.x + mIndicatorInterval * mOffSet;
+
+                path1 = calculateUnderHalfPath(indicator1);
+
+                indicator2.point8.x = indicator22.point8.x - mIndicatorInterval * mOffSet;
+                indicator2.point9.x = indicator22.point8.x - mIndicatorInterval * mOffSet;
+                indicator2.point10.x = indicator22.point8.x - mIndicatorInterval * mOffSet;
+
+                path2 = calculateUnderHalfPath(indicator2);
+
+            } else if (mCurrentIndicatorPosition == 1) {
+                indicator2.point2.x = indicator22.point2.x + mIndicatorInterval * mOffSet;
+                indicator2.point3.x = indicator22.point2.x + mIndicatorInterval * mOffSet;
+                indicator2.point4.x = indicator22.point2.x + mIndicatorInterval * mOffSet;
+
+                path2 = calculateUnderHalfPath(indicator2);
+
+                indicator3.point8.x = indicator33.point8.x - mIndicatorInterval * mOffSet;
+                indicator3.point9.x = indicator33.point8.x - mIndicatorInterval * mOffSet;
+                indicator3.point10.x = indicator33.point8.x - mIndicatorInterval * mOffSet;
+
+                path3 = calculateUnderHalfPath(indicator3);
+            } else if (mCurrentIndicatorPosition == 2) {
+                indicator3.point2.x = indicator33.point2.x + mIndicatorInterval * mOffSet;
+                indicator3.point3.x = indicator33.point2.x + mIndicatorInterval * mOffSet;
+                indicator3.point4.x = indicator33.point2.x + mIndicatorInterval * mOffSet;
+
+                path3 = calculateUnderHalfPath(indicator3);
+
+                indicator4.point8.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
+                indicator4.point9.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
+                indicator4.point10.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
+
+                path4 = calculateUnderHalfPath(indicator4);
+            } else if (mCurrentIndicatorPosition == 3) {
+                indicator4.point2.x = indicator44.point2.x + mIndicatorInterval * mOffSet;
+                indicator4.point3.x = indicator44.point2.x + mIndicatorInterval * mOffSet;
+                indicator4.point4.x = indicator44.point2.x + mIndicatorInterval * mOffSet;
+
+                path4 = calculateUnderHalfPath(indicator4);
+
+                indicator5.point8.x = indicator55.point8.x - mIndicatorInterval * mOffSet;
+                indicator5.point9.x = indicator55.point8.x - mIndicatorInterval * mOffSet;
+                indicator5.point10.x = indicator55.point8.x - mIndicatorInterval * mOffSet;
+
+                path5 = calculateUnderHalfPath(indicator5);
+            } else if (mCurrentIndicatorPosition == 4) {
+                indicator5.point2.x = indicator55.point2.x + mIndicatorInterval * mOffSet;
+                indicator5.point3.x = indicator55.point2.x + mIndicatorInterval * mOffSet;
+                indicator5.point4.x = indicator55.point2.x + mIndicatorInterval * mOffSet;
+
+                path5 = calculateUnderHalfPath(indicator5);
+
+                indicator4.point8.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
+                indicator4.point9.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
+                indicator4.point10.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
+
+                path4 = calculateUnderHalfPath(indicator4);
+            }
         } else {
-            delt = deltBigger;
+            if (mCurrentIndicatorPosition == 0){
+                path1 = calculateAboveHalfPath(indicator1,indicator2);
+            } else if(mCurrentIndicatorPosition == 1){
+                path2 = calculateAboveHalfPath(indicator2,indicator3);
+            } else if(mCurrentIndicatorPosition == 2){
+                path3 = calculateAboveHalfPath(indicator3,indicator4);
+            } else if(mCurrentIndicatorPosition == 3){
+                path4 = calculateAboveHalfPath(indicator4,indicator5);
+            } else if(mCurrentIndicatorPosition == 4){
+                path4 = calculateAboveHalfPath(indicator5,indicator4);
+            }
         }
-
-        if (mCurrentIndicatorPosition == 0) {
-            indicator1.point2.x = indicator11.point2.x + mIndicatorInterval * mOffSet;
-            indicator1.point3.x = indicator11.point2.x + mIndicatorInterval * mOffSet;
-            indicator1.point4.x = indicator11.point2.x + mIndicatorInterval * mOffSet;
-
-            path1 = calculatePath(indicator1);
-
-            indicator2.point8.x = indicator22.point8.x - mIndicatorInterval * mOffSet;
-            indicator2.point9.x = indicator22.point8.x - mIndicatorInterval * mOffSet;
-            indicator2.point10.x = indicator22.point8.x - mIndicatorInterval * mOffSet;
-
-            path2 = calculatePath(indicator2);
-
-        } else if (mCurrentIndicatorPosition == 1) {
-            indicator2.point2.x = indicator22.point2.x + mIndicatorInterval * mOffSet;
-            indicator2.point3.x = indicator22.point2.x + mIndicatorInterval * mOffSet;
-            indicator2.point4.x = indicator22.point2.x + mIndicatorInterval * mOffSet;
-
-            path2 = calculatePath(indicator2);
-
-            indicator3.point8.x = indicator33.point8.x - mIndicatorInterval * mOffSet;
-            indicator3.point9.x = indicator33.point8.x - mIndicatorInterval * mOffSet;
-            indicator3.point10.x = indicator33.point8.x - mIndicatorInterval * mOffSet;
-
-            path3 = calculatePath(indicator3);
-        } else if (mCurrentIndicatorPosition == 2) {
-            indicator3.point2.x = indicator33.point2.x + mIndicatorInterval * mOffSet;
-            indicator3.point3.x = indicator33.point2.x + mIndicatorInterval * mOffSet;
-            indicator3.point4.x = indicator33.point2.x + mIndicatorInterval * mOffSet;
-
-            path3 = calculatePath(indicator3);
-
-            indicator4.point8.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
-            indicator4.point9.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
-            indicator4.point10.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
-
-            path4 = calculatePath(indicator4);
-        } else if (mCurrentIndicatorPosition == 3) {
-            indicator4.point2.x = indicator44.point2.x + mIndicatorInterval * mOffSet;
-            indicator4.point3.x = indicator44.point2.x + mIndicatorInterval * mOffSet;
-            indicator4.point4.x = indicator44.point2.x + mIndicatorInterval * mOffSet;
-
-            path4 = calculatePath(indicator4);
-
-            indicator5.point8.x = indicator55.point8.x - mIndicatorInterval * mOffSet;
-            indicator5.point9.x = indicator55.point8.x - mIndicatorInterval * mOffSet;
-            indicator5.point10.x = indicator55.point8.x - mIndicatorInterval * mOffSet;
-
-            path5 = calculatePath(indicator5);
-        } else if (mCurrentIndicatorPosition == 4) {
-            indicator5.point2.x = indicator55.point2.x + mIndicatorInterval * mOffSet;
-            indicator5.point3.x = indicator55.point2.x + mIndicatorInterval * mOffSet;
-            indicator5.point4.x = indicator55.point2.x + mIndicatorInterval * mOffSet;
-
-            path5 = calculatePath(indicator5);
-
-            indicator4.point8.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
-            indicator4.point9.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
-            indicator4.point10.x = indicator44.point8.x - mIndicatorInterval * mOffSet;
-
-            path4 = calculatePath(indicator4);
-        }
-
         invalidate();
     }
 
