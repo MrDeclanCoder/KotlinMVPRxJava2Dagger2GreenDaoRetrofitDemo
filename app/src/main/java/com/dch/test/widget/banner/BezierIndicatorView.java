1959ecc7b4fc4ecbd604495529e598af2159dc96
MrDeclanCoder <dch123654>
2017-08-01 08:25:45 +0800

    自定义贝塞尔效果指示器indicator

--- app/src/main/java/com/dch/test/widget/banner/BezierIndicatorView.java
+++ app/src/main/java/com/dch/test/widget/banner/BezierIndicatorView.java
@@ -179,8 +179,8 @@ public class BezierIndicatorView extends View implements ViewPager.OnPageChangeL
 
     private Path calculateAboveHalfPath(Indicator preIndicator, Indicator nextIndicator) {
         float middleX = (preIndicator.point2.x + mIndicatorInterval/2);
-        float topY = mHeight/2 -  mOffSet * mIndicatorRadius;
-        float bottomY = mHeight/2 + mOffSet* mIndicatorRadius;
+        float topY = mHeight/2 -  mOffSet * mIndicatorRadius*0.9f;
+        float bottomY = mHeight/2 + mOffSet* mIndicatorRadius*0.9f;
         Path mBezierPath = new Path();
         mBezierPath.moveTo(preIndicator.point0.x, preIndicator.point0.y);
         mBezierPath.quadTo(middleX,bottomY,nextIndicator.point0.x,nextIndicator.point0.y);
